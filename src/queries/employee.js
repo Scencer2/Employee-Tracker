4ab7ba00d601a998bc343374085b1729b3236d9d
Scencer2 <Scencer2@yahoo.com>
2025-02-26 23:46:36 -0800

    employee and role queries

---
+++ src/queries/employee.js
@@ -0,0 +1,13 @@
+import pool from '../connection.js';
+
+export const getAllEmployees = async () => {
+  const res = await pool.query('SELECT * FROM employee');
+  return res.rows;
+};
+
+export const addEmployee = async (firstName, lastName, roleId, managerId) => {
+  const res = await pool.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4) RETURNING *', [firstName, lastName, roleId, managerId]);
+  return res.rows[0];
+};
+
+
---
+++ src/queries/role.js
@@ -0,0 +1,11 @@
+import pool from '../connection.js';
+
+export const getAllRoles = async () => {
+  const res = await pool.query('SELECT * FROM role');
+  return res.rows;
+};
+
+export const addRole = async (title, salary, departmentId) => {
+  const res = await pool.query('INSERT INTO role (title, salary, department_id) VALUES ($1, $2, $3) RETURNING *', [title, salary, departmentId]);
+  return res.rows[0];
+};
