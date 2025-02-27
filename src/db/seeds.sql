a615d988f21819fdabb0ea52c9cc0bdf5807d469
Scencer2 <Scencer2@yahoo.com>
2025-02-26 22:10:24 -0800

    added seom stuff to seeds

--- src/db/seeds.sql
+++ src/db/seeds.sql
@@ -5,11 +5,14 @@ INSERT INTO department (name) VALUES
 
 INSERT INTO role (title, salary, department_id) VALUES
 ('Sales Manager', 80000, 1),
+('Salesperson', 60000, 1),
 ('HR Manager', 70000, 2),
-('Director of Marketing', 110000, 3);
+('HR assistant', 45000, 2),
+('Director of Marketing', 110000, 3),
+('Marketing Analyst', 70000, 3)
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
 ('John', 'Doe', 1, NULL),
-('Mike', 'Chan', 2, 1)
+('Mike', 'Chan', 2, NULL),
 ('Ashley', 'Rodriguez', 3, NULL),
 ('Kevin', 'Tupik', 1, 1);
